49e6f2c5d4c45f5a5b23c058e08042e8bd8d73a4
Andrzej Zareba <zareba.andrew@gmail.com>
2020-05-17 00:32:25 +0200

    add automatic column generation; add controls; tweak app styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import dataGenerator from './utils/dataGenerator'
+import Table from './components/Table/Table'
+
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [columnsCount, setColumnsCount] = useState(5)
+  const [rowsCount, setRowsCount] = useState(10)
+  const { columns, data } = dataGenerator(columnsCount, rowsCount)
+
   return (
     <div className="App">
-      
+      <div className="App__controls">
+        <label>
+          Columns: {columnsCount}
+          <input type='range' min='1' max='25' value={columnsCount} onChange={event => setColumnsCount(parseInt(event.target.value))} />
+        </label>
+        <label>
+          Rows: {rowsCount}
+          <input type='range' min='1' max='200' value={rowsCount} onChange={event => setRowsCount(parseInt(event.target.value))} />
+        </label>
+      </div>
+      <Table data={data} columns={columns} />
     </div>
   );
 }
--- src/utils/dataGenerator.ts
+++ src/utils/dataGenerator.ts
@@ -1,12 +1,31 @@
 import faker from 'faker'
+import { IColumn } from '../components/Table/Table'
 
-export const dataGenerator = (columns: number, rows: number): string[][] => {
+const dataGenerator = (columnsCount: number, rowsCount: number): { data: string[][], columns: IColumn[] } => {
   const data: string[][] = []
-  for (let i = 0; i < rows; i++) {
+  const columns: IColumn[] = []
+
+  for (let i = 0; i < rowsCount; i++) {
     data.push([])
-    for(let j = 0; j < columns; j++) {
+    for (let j = 0; j < columnsCount; j++) {
       data[i].push(faker.random.word())
     }
   }
-  return data
+
+  for (let i = 0; i < columnsCount; i++) {
+    columns.push({
+      name: faker.random.word(),
+      width: faker.random.number({
+        min: 100,
+        max: 200
+      })
+    })
+  }
+
+  return {
+    data,
+    columns
+  }
 }
+
+export default dataGenerator
